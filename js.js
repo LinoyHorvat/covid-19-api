85f0418f17ce0552e8441d805d57cd077b72e359
LinoyHorvat <linoy.horvat@gmail.com>
2021-12-09 17:43:18 +0200

    first commit

--- js.js
+++ js.js
@@ -4,61 +4,83 @@ const regionsCountries = {
   africa: [],
   americas: [],
   world: [],
-}
-const regions = ['asia', 'europe', 'africa', 'americas'];
-
+};
+const regions = ["asia", "europe", "africa", "americas"];
 
 const world = {
   asia: {},
   europe: {},
   africa: {},
-  americas: {}
+  americas: {},
+};
+
+const asiaBtn = document.querySelector("button");
+
+asiaBtn.addEventListener("click", () => {
+  let region = "asia";
+  if (Object.keys(world[region]).length === 0) {
+    getCountriesByRegion(region);
+  }
+  else console.log("not inside");
+});
+
+async function getCountriesByRegion(region) {
+  console.log("in getCountriesByRegion");
+  try {
+    const result = await fetch(
+      `https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${region}`
+    );
+    const data = await result.json();
+    data.forEach((country) => {
+      regionsCountries[region].push(country.cca2);
+      world[region][country.cca2] = {};
+      fetchCovidData(region, country.cca2, country.name.common);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-async function getCountriesByRegion(region){
-  if (regionsCountries[region]){
-    try{
-      const result = await fetch(`https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${region}`);
-      const data = await result.json();
-      data.forEach(country => {
-        regionsCountries[region].push(country.cca2);
-        world[region][country.cca2] = {}
-        fetchCovidData(region,country.cca2,country.name.common);
-      })
-    }
-    catch(err) {console.log(err);}
+async function fetchCovidData(region, country, name) {
+  try {
+    const result = await fetch(
+      `https://intense-mesa-62220.herokuapp.com/http://corona-api.com/countries/${country}`
+    );
+    const data = await result.json();
+    const { confirmed, deaths, critical, recovered } = data.data.latest_data;
+    const { confirmed: newCases, deaths: newDeaths } = data.data.today;
+    buildWorldObj(
+      region,
+      country,
+      name,
+      confirmed,
+      newCases,
+      deaths,
+      newDeaths,
+      recovered,
+      critical
+    );
+  } catch (err) {
+    console.log(err);
   }
-  console.log(regionsCountries);
-  }
+}
 
-  async function fetchCovidData(region,country,name) {
-    try{
-      const result = await fetch(`https://intense-mesa-62220.herokuapp.com/http://corona-api.com/countries/${country}`);
-      const data = await result.json();
-      const {
-        confirmed,
-        deaths,
-        critical,
-        recovered,
-      } = data.data.latest_data;
-
-      const {
-        confirmed: newCases,
-        deaths: newDeaths
-      } = data.data.today
-      buildWorldObj(region, country, name, confirmed, newCases, deaths, newDeaths, recovered, critical);
-
-    }    
-    catch(err) {console.log(err);}
-  }
-
-  function buildWorldObj(region,country,name,totalCases,newCases,totalDeaths,newDeaths,totalRecovered,inCritical){
-    world[region][country]['name'] = name;
-    world[region][country]['totalCases'] = totalCases;
-    world[region][country]['newCases'] = newCases;
-    world[region][country]['totalDeaths'] = totalDeaths;
-    world[region][country]['newDeaths'] = newDeaths;
-    world[region][country]['totalRecovered'] = totalRecovered;
-    world[region][country]['inCritical'] = inCritical;
-  }
-  getCountriesByRegion(regions[0]);
+function buildWorldObj(
+  region,
+  country,
+  name,
+  totalCases,
+  newCases,
+  totalDeaths,
+  newDeaths,
+  totalRecovered,
+  inCritical
+) {
+  world[region][country]["name"] = name;
+  world[region][country]["totalCases"] = totalCases;
+  world[region][country]["newCases"] = newCases;
+  world[region][country]["totalDeaths"] = totalDeaths;
+  world[region][country]["newDeaths"] = newDeaths;
+  world[region][country]["totalRecovered"] = totalRecovered;
+  world[region][country]["inCritical"] = inCritical;
+}
